b1619c12140fd28292969b8af95fa549a423e123
AaryanBasnet <basnetaryan1011@gmail.com>
2026-01-25 16:25:26 +0545

    chore: export all hooks from central barrel

---
+++ frontend/src/hooks/index.js
@@ -0,0 +1,4 @@
+export { useProducts } from './useProducts';
+export { useGenderProducts } from './useGenderProducts';
+export { useProductDetail } from './useProductDetail';
+export { useReviews } from './useReviews';
