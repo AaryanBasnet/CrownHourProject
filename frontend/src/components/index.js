3a4366712c2d1cdf473809f877de6a99b7f9bc33
AaryanBasnet <basnetaryan1011@gmail.com>
2026-01-25 16:25:26 +0545

    chore: export all components from central barrel

---
+++ frontend/src/components/index.js
@@ -0,0 +1,16 @@
+/**
+ * Components Export
+ */
+
+// Layout
+export { Navbar } from './layout/Navbar';
+
+// Auth
+export { RequireAuth, RequireRole, PublicOnly } from './auth';
+
+// Product
+export { ProductCard } from './product/ProductCard';
+
+// Common
+export { ErrorBoundary } from './common/ErrorBoundary';
+export { LoadingSpinner } from './common/LoadingSpinner';
