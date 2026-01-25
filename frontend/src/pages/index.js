fb5592066e1f90fb29377eaab6d5571a2024d52d
AaryanBasnet <basnetaryan1011@gmail.com>
2026-01-25 16:25:26 +0545

    chore: export all pages from central barrel

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,7 @@
+export { Shop } from './Shop';
+export { Men } from './Men';
+export { Women } from './Women';
+export { ProductDetail } from './ProductDetail';
+export { Wishlist } from './Wishlist';
+export { Cart } from './Cart';
+export { Home } from './Home';
