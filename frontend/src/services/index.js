6876ebb82df1204a08e305de10bbd3ed395e4230
AaryanBasnet <basnetaryan1011@gmail.com>
2026-01-25 16:25:26 +0545

    chore: export all services from central barrel

---
+++ frontend/src/services/index.js
@@ -0,0 +1,12 @@
+/**
+ * Service Layer Exports
+ * Centralized export for all API services
+ */
+
+export { authService } from './authService';
+export { productService } from './productService';
+export { genderProductService } from './genderProductService';
+export { reviewService } from './reviewService';
+export { orderService } from './orderService';
+export { cartService, wishlistService } from './cartAndWishlistService';
+export { paymentService } from './paymentService';
